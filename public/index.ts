ba67e9e2ffc8019dcd044341131c4dca81848df8
rudra <goutam.prasad.2707@gmail.com>
2025-10-11 20:55:55 +0530

    Add images and logo assets for the project

---
+++ public/index.ts
@@ -0,0 +1,63 @@
+import logo from "./images/logo.svg";
+
+import hero_bg from "./images/hero_bg.jpg";
+import hero_1 from "./images/hero_1.jpg";
+import hero_2 from "./images/hero_2.jpg";
+
+import profile_1 from "./images/profile_1.jpg";
+import profile_2 from "./images/profile_2.jpg";
+import profile_3 from "./images/profile_3.jpg";
+
+import featured_memoir_1 from "./images/featured_memoir_1.jpg";
+import featured_memoir_2 from "./images/featured_memoir_2.jpg";
+import featured_memoir_3 from "./images/featured_memoir_3.jpg";
+import featured_memoir_4 from "./images/featured_memoir_4.jpg";
+import featured_memoir_5 from "./images/featured_memoir_5.jpg";
+import featured_memoir_6 from "./images/featured_memoir_6.jpg";
+import featured_memoir_7 from "./images/featured_memoir_7.jpg";
+
+import services_1 from "./images/services_1.jpg";
+import services_2 from "./images/services_2.jpg";
+import services_3 from "./images/services_3.jpg";
+
+import gallery_memoir_1 from "./images/gallery_memoir_1.jpg";
+import gallery_memoir_2 from "./images/gallery_memoir_2.jpg";
+import gallery_memoir_3 from "./images/gallery_memoir_3.jpg";
+import gallery_memoir_4 from "./images/gallery_memoir_4.jpg";
+import gallery_memoir_5 from "./images/gallery_memoir_5.jpg";
+import gallery_memoir_6 from "./images/gallery_memoir_6.jpg";
+import gallery_memoir_7 from "./images/gallery_memoir_7.jpg";
+import gallery_memoir_8 from "./images/gallery_memoir_8.jpg";
+import gallery_memoir_9 from "./images/gallery_memoir_9.jpg";
+
+import contact_bg from "./images/contact_bg.jpg";
+
+export {
+  logo,
+  hero_1,
+  hero_2,
+  hero_bg,
+  profile_1,
+  profile_2,
+  profile_3,
+  services_1,
+  services_2,
+  services_3,
+  gallery_memoir_1,
+  gallery_memoir_2,
+  gallery_memoir_3,
+  gallery_memoir_4,
+  gallery_memoir_5,
+  gallery_memoir_6,
+  gallery_memoir_7,
+  gallery_memoir_8,
+  gallery_memoir_9,
+  contact_bg,
+  featured_memoir_1,
+  featured_memoir_2,
+  featured_memoir_3,
+  featured_memoir_4,
+  featured_memoir_5,
+  featured_memoir_6,
+  featured_memoir_7,
+};
